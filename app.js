951d5f160b17f96e18d2552db6c8e04e02621aa0
sukscode <sukritisrivastava02@gmail.com>
2021-03-01 01:37:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 console.log(btnTranslate);
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-btnTranslate.addEventListener("click", function clickEventHandler() {
-  console.log("clicked");
-});
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+function getTranslationUrl(text) {
+  return serverURL + "?" + "text=" + text;
+}
+//calling server for processing
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something went wrong !!");
+}
+function clickHandler() {
+  var inputText = txtInput.Value;
+
+  fetch(getTranslationUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click", clickHandler);
