b22a1df98c2ba90052eaecd3ceb3d959042047b9
sukscode <sukritisrivastava02@gmail.com>
2021-02-28 23:39:00 +0530

    output and input

---
+++ app.js
@@ -0,0 +1,7 @@
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+btnTranslate.addEventListener("click", function clickEventHandler() {
+  console.log("clicked");
+});
